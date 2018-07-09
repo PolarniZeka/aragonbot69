4ddf2fdb280f2d095d271332642ff6968b0f1468
PolarniZeka <40997937+PolarniZeka@users.noreply.github.com>
2018-07-09 13:18:33 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+
+const bot = new Discord.Client({disableEveryone: true});
+
+bot.on("ready", async () => {
+  console.log(`${bot.user.username} is online!`);
+
+  bot.user.setActivity("pornography", {type: "WATCHING"});
+
+);
+});
+
+});
+
+client.login(procces.env.BOT_TOKEN);
